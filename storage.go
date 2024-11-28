f081d2aafe147f797001c7746b35bff8541af1e4
Supriyo Paul <paulsupriyo64nk@gmail.com>
2024-11-28 17:15:25 +0000

    storage write, read, delete with hashed folder paths

--- storage.go
+++ storage.go
@@ -1,19 +1,54 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+const root = "hmzNetwork"
+
+// NOTE: Lots of types of transform funcs can be added, like git paths etc. etc..
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	FileName string
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
+	Root              string
 }
 
-var DefaultPathTransformFunc = func(key string) string {
-	return key
+var DefaultPathTransformFunc = func(key string) PathKey {
+	return PathKey{key, key}
+}
+
+func (p PathKey) FullPath() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.FileName)
+}
+
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key)) // [20]byte => []byte => [:]
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashStr[from:to]
+	}
+
+	return PathKey{PathName: strings.Join(paths, "/"), FileName: hashStr}
 }
 
 type Store struct {
@@ -21,28 +56,72 @@ type Store struct {
 }
 
 func NewStore(opts StoreOpts) *Store {
+	if opts.PathTransformFunc == nil {
+		opts.PathTransformFunc = DefaultPathTransformFunc
+	}
+	if len(opts.Root) == 0 {
+		opts.Root = root
+	}
 	return &Store{
 		StoreOpts: opts,
 	}
 }
 
+func (s *Store) HasKey(key string) bool {
+	pathKey := s.PathTransformFunc(key)
+
+	_, err := os.Stat(pathKey.FullPath())
+	return err == fs.ErrNotExist
+}
+
+func (s *Store) Delete(key string) error {
+	pathKey := s.PathTransformFunc(key)
+
+	defer func() {
+		log.Printf("deleted [%s] from disk", pathKey.FileName)
+	}()
+
+	return os.Remove(pathKey.FullPath())
+}
+
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.ReadStream(key)
+	if err != nil {
+		return nil, nil
+	}
+
+	defer f.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+
+	return buf, err
+}
+
+func (s *Store) ReadStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
+}
+
 func (s *Store) WriteStream(key string, r io.Reader) error {
-	pathName := s.PathTransformFunc(key)
+	pathKey := s.PathTransformFunc(key)
+	pathName := pathKey.PathName
 
 	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	fileName := "someFileName"
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
 
-	pathAndFileName := pathName + "/" + fileName
+	pathAndFileName := pathKey.FullPath()
 
-	f, err := os.Create(pathAndFileName)
+	file, err := os.Create(pathAndFileName)
 	if err != nil {
 		return err
 	}
 
-	n, err := io.Copy(f, r)
+	n, err := io.Copy(file, buf)
 	if err != nil {
 		return err
 	}
--- store_test.go
+++ store_test.go
@@ -2,17 +2,58 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"testing"
 )
 
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestdish"
+	pathKey := CASPathTransformFunc(key)
+	pathName := pathKey.PathName
+	expectedPathName := "89e4c/9a0d6/7ba03/85064/c3ff7/5ec0b/b3f27/af2b5"
+	FileNameKey := "89e4c9a0d67ba0385064c3ff75ec0bb3f27af2b5"
+
+	if pathName != expectedPathName {
+		t.Errorf("Expected path name: %s\n Actual path name:%s\n", expectedPathName, pathName)
+	}
+
+	if pathKey.FileName != FileNameKey {
+		t.Errorf("Expected FileName Key: %s\n Actual path key:%s\n", FileNameKey, pathKey.FileName)
+	}
+}
+
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 	s := NewStore(opts)
+	key := "specialPicture"
+	content := []byte("Hello Hunny Bunny!!")
+	data := bytes.NewReader(content)
 
-	data := bytes.NewReader([]byte("Some jpg bytes.."))
-	if err := s.WriteStream("myPicture", data); err != nil {
+	if err := s.WriteStream(key, data); err != nil {
 		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, err := io.ReadAll(r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(b) != string(content) {
+		t.Errorf("Expected: %s, Got: %s\n", string(content), string(b))
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+
+	if s.HasKey(key) {
+		t.Errorf("key [%s] should not exist after delete operation!", key)
 	}
 }
